70cc70bc3c2a5b85e79773bbb22decccd7c40b2a
Liseth Gutierrez <liseth.yggdrasil@gmail.com>
2020-11-09 10:15:22 -0400

    test

--- app/services/websockets/binance_websocket.py
+++ app/services/websockets/binance_websocket.py
@@ -10,27 +10,22 @@ import json
 class Handler:
 
     def __init__(self):
-        # self.command = attr
         self.URL = BINANCE_URL
-        # self.loop = None
-        # self.loop.run_until_complete(self.__async_connect())    #Connection to Binance Websocket
-        # self.start_process()
         self.ws = None
         self.loop = asyncio.get_event_loop()
         self.loop.run_until_complete(self.__async_connect())
         self.start_process()
-
-    # def lol(self):
-    #     self.loop = asyncio.get_event_loop()
-    #     # if self.command=='up':
-    #     self.URL = BINANCE_URL
-    #     self.loop.run_until_complete(self.__async_connect())
-    #     self.start_process()
+        self.flag = False
 
     async def __async_connect(self):
         print("attempting connection to {}".format(self.URL))
-        self.ws = await connect(self.URL)
-        print("connected")
+        try:
+            self.flag = Parameters.objects.get(websocketActive=True)
+            self.ws = await connect(self.URL)
+            print("connected")
+        except Exception as e:
+            print("Error en la conexion")
+            raise Exception(e)
 
     def constructURL(self, coins):
         for i in coins:
@@ -41,15 +36,15 @@ class Handler:
 
     def start_process(self):
         service_bus.init_connection()
-        # flag = True if self.command=='up' else False
 
-        while True:
-            response = self.__socket_response()   #Socket response
+        while self.flag['websocketActive']:
+            response = self.__socket_response()
             coins = service_bus.receive('coins', response)
-            flag = Parameters.objects.get(websocketActive=True)
-            if flag['websocketActive']!=True:
-                print(flag['websocketActive'])
+            try:
+                flag = Parameters.objects.get(websocketActive=True)
+            except Exception as e:
                 self.__close_connection()
+                raise Exception(e)
 
     def __socket_response(self):
         return self.loop.run_until_complete(self.__async_get_data())
